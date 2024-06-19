24d8776dbbe21dd340eb33775ab161026fc05c69
Ismael <ismeyumi@espol.edu.ec>
2024-06-18 19:20:49 -0500

    Inicio proyecto

---
+++ src/main/java/com/company/proyectoprueba/ProyectoPrueba.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.company.proyectoprueba;
+
+/**
+ *
+ * @author Yumi
+ */
+public class ProyectoPrueba {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
